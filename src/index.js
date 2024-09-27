a956204e0a14011c03923515f67c7a814ecf4dc4
Gleb <glebshm@rambler.ru>
2024-09-27 14:07:25 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj=new Map(bracketsConfig)
+  let stack=[]
+  str.split('').map((item)=>{
+    let last=stack[stack.length-1]
+    if (item===obj.get(last)) {
+      stack.pop()
+    } else {
+      stack.push(item)
+    }
+  })
+  return stack.length===0
 }
+
